fef7ed4074f516a0339684188f0547074427c4ee
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2021-07-09 13:30:20 +0000

    Fix styling

--- src/BladeRPGAwesomeIconsServiceProvider.php
+++ src/BladeRPGAwesomeIconsServiceProvider.php
@@ -19,7 +19,6 @@ final class BladeRPGAwesomeIconsServiceProvider extends ServiceProvider
 
             $factory->add('rpg-awesome-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
-
     }
 
     private function registerConfig(): void
