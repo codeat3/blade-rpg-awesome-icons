5254de983d546d98fd6002d7d979d90730e350a4
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-07-26 05:24:39 +0000

    Fix styling

--- src/BladeRPGAwesomeIconsServiceProvider.php
+++ src/BladeRPGAwesomeIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeRPGAwesomeIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-rpg-awesome-icons', []);
 
-            $factory->add('rpg-awesome-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('rpg-awesome-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-rpg-awesome-icons.php', 'blade-rpg-awesome-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-rpg-awesome-icons.php', 'blade-rpg-awesome-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-rpg-awesome-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-rpg-awesome-icons'),
             ], 'blade-rpg'); // TODO: update the alias to `blade-rpg-awesome-icons` for better readability in next major release
 
             $this->publishes([
-                __DIR__.'/../config/blade-rpg-awesome-icons.php' => $this->app->configPath('blade-rpg-awesome-icons.php'),
+                __DIR__ . '/../config/blade-rpg-awesome-icons.php' => $this->app->configPath('blade-rpg-awesome-icons.php'),
             ], 'blade-rpg-awesome-icons-config');
         }
     }
